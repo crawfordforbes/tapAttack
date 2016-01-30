0becdacff75e45c7e6475a3dbff7950d642ce5c1
shunter <shunter@dapperhat.net>
2016-01-30 16:33:41 -0700

    Playback!

--- public/js/tapAttack.js
+++ public/js/tapAttack.js
@@ -128,6 +128,14 @@ var roundProto = {
    //this is in its own object, for easier reasoning about
 };
 
+///////////////////////////////
+//  Gameplay Playback State  //
+///////////////////////////////
+
+TapApp.playbackIndex = 0;
+TapApp.playbackStartTime = 0;
+TapApp.playbackNoteIndex = 0;
+
 /////////////////
 //  Constants  // 
 /////////////////
@@ -306,7 +314,7 @@ function setState(newState) {
 	if(newState === TapApp.playback_state) {
 		var d = new Date();
 		console.log("Switched state to playback state at time " + d.getTime() + ", starting playback");
-		startPlayback();
+		//startPlayback();
 	// stop playback
 	} 
 	for(var i = 0; i < TapApp.stateButtonArray.length; i++) {
@@ -758,6 +766,53 @@ var indicate_state = function(state, ctx) {
 };
 
 
+function startPlaybackRound()
+{
+
+}
+function playNextNote()
+{
+	var roundResult = TapApp.roundResult[TapApp.playbackIndex];
+	var note = roundResult.notes[TapApp.playbackNoteIndex];
+	playRegion(TapApp.regionSet.regionArray[note.region]);
+	++TapApp.playbackNoteIndex;
+
+	var waitTime = 1000;
+	if (TapApp.playbackNoteIndex === roundResult.notes.length)
+	{
+		++TapApp.playbackIndex;
+		if (TapApp.playbackIndex === TapApp.roundResult.length)
+		{
+			setState(TapApp.freeplay_state);
+			startGame();
+		}
+		else
+		{
+			setTimeout(function() {
+				runGameplayPlayback();
+			}, waitTime);
+		}
+	}
+	else
+	{
+		var nextNote = roundResult.notes[TapApp.playbackNoteIndex];
+		waitTime = nextNote.time - note.time;
+		setTimeout(function() {
+			playNextNote();
+		}, waitTime);		
+	}
+}
+
+function runGameplayPlayback()
+{
+	var d = new Date();
+
+	TapApp.playbackStartTime = d.getTime();
+	TapApp.playbackNoteIndex = 0;
+	
+	playNextNote();
+}
+
 function incrementRound()
 {
 	if (TapApp.currentRoundResult !== null)
@@ -774,6 +829,10 @@ function incrementRound()
 		if (TapApp.roundNumber == rounds)
 		{
 			console.log("Game is over here but we're going to just keep going for now");		
+
+			//HACK HACK HACK
+			setState(TapApp.playback_state);
+			runGameplayPlayback();
 		}		
 	}	
 }
@@ -792,12 +851,8 @@ function nextRound()
 }
 
 //todo: give this a better home
-function playRegion(region)
+function playRegionGameplayUpdate(region)
 {
-	if(TapApp.state === TapApp.recording_state)
-		region.record();
-
-
     if (TapApp.round !== undefined)
     {
     	if (TapApp.round.startTime === undefined)
@@ -820,8 +875,18 @@ function playRegion(region)
 	    	note.time = d.getTime() - TapApp.round.startTime;
 	    	TapApp.currentRoundResult.notes.push(note);
 	    }    	
-    }
+    }	
+}
 
+function playRegion(region)
+{
+	if(TapApp.state === TapApp.recording_state)
+		region.record();
+
+	if (TapApp.state === TapApp.freeplay_state)
+	{
+		playRegionGameplayUpdate(region);
+	}
 
 	region.play();	
 }
@@ -985,6 +1050,8 @@ function startRound() {
 
 function startGame()
 {
+	TapApp.roundNumber = 0;
+	TapApp.currentPlayer = 0;
 	nextRound();
 }
 
